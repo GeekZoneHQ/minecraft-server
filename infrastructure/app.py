a2bbac56739bf1ec8b8e7769ac69841045001ee8
Carwyn Nelson <carwyn@carwynnelson.com>
2020-06-06 12:32:28 +0100

    Setup the minecraft server on boot
    This uses the EC2 userdata feature to download and configure spigot on
    boot. The downside of the approach is that it takes a while for an EC2
    to complete setup. This can be sped up by generating an AMI of the
    server which might be a future improvement.
    
    I have set this up to download the world data from S3 which should be
    a good basis for backups and whole-system-rebuilds.
    
    The S3 bucket is in a separate stack to avoid new buckets being
    created when we update the minecraft-server stack. This means we don't
    have to shuffle data between buckets. Also cloudformation does not
    delete S3 buckets when the stack is deleted to protect data.
    
    I am using systemd to run the minecraft server. This is more robust
    than running a shell script in a screen by hand, but could certainly
    be improved with some custom scripting, plugins, or an off the shelf
    game server control panel.

--- infrastructure/app.py
+++ infrastructure/app.py
@@ -2,11 +2,15 @@
 
 from aws_cdk import core
 
-from stacks.minecraft_server import MinecraftServer
+from stacks.minecraft_server import (
+    MinecraftServer,
+    BackupBucket
+)
 
 app = core.App()
-MinecraftServer(app, "minecraft-server", env={
-    'region': 'eu-west-2',
-    'account': '098281131088'})
+env = env={'region': 'eu-west-2', 'account': '098281131088'}
+
+bucket = BackupBucket(app, "minecraft-backup", env=env)
+MinecraftServer(app, "minecraft-server", env=env, backup_bucket=bucket.backup_bucket)
 
 app.synth()
--- infrastructure/stacks/minecraft_server.py
+++ infrastructure/stacks/minecraft_server.py
@@ -1,10 +1,19 @@
 from aws_cdk import (
     aws_ec2 as ec2,
+    aws_s3 as s3,
+    aws_iam as iam,
     core
 )
+from aws_cdk.aws_ec2 import UserData
+
+class BackupBucket(core.Stack):
+    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+        super().__init__(scope, id, **kwargs)
+        self.backup_bucket = s3.Bucket(self, "mc-backup")
+
 
 class MinecraftServer(core.Stack):
-    def __init__(self, scope: core.Construct, id: str, **kwargs) -> None:
+    def __init__(self, scope: core.Construct, id: str, backup_bucket, **kwargs) -> None:
         super().__init__(scope, id, **kwargs)
 
         ubuntu_lts = ec2.MachineImage.generic_linux({
@@ -20,10 +29,21 @@ class MinecraftServer(core.Stack):
         security_group.add_ingress_rule(ec2.Peer.any_ipv4(),
             ec2.Port.tcp(25565), "Spigot port")
 
-        ec2.Instance(self, "MinecraftEC2",
+        role = iam.Role(self, "MinecraftServerRole",
+            assumed_by=iam.ServicePrincipal("ec2.amazonaws.com"))
+        role.add_to_policy(iam.PolicyStatement(
+            resources=[
+                backup_bucket.bucket_arn,
+                backup_bucket.bucket_arn + "/*"
+            ],
+            actions=["s3:GetObject", "s3:ListBucket"]))
+
+        instance = ec2.Instance(self, "MinecraftEC2",
             instance_type=ec2.InstanceType("t3.small"),
             machine_image=ubuntu_lts,
             vpc=vpc,
             security_group=security_group,
-            key_name="carwyn")
-        
+            key_name="carwyn",
+            role=role)
+        setup_file = open("./configure_minecraft.sh", "rb").read()
+        instance.user_data.add_commands(str(setup_file, 'utf-8'))
